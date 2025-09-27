9b34a46081af8c089d9892bcf29e92cb0f8c4d73
00346112 <agusdwiismawan@corp.bri.co.id>
2025-09-27 16:23:02 +0700

    change rsvp formated date

--- src/js/rsvp.js
+++ src/js/rsvp.js
@@ -17,6 +17,16 @@ export function setupRSVP() {
         document.getElementById("popup-alert").style.display = "none";
     }
 
+    const getCurrentDateTime = () => {
+        const now = new Date();
+        let year = now.getFullYear();
+        let month = (now.getMonth() + 1).toString().padStart(2, '0');
+        let day = now.getDate().toString().padStart(2, '0');
+        let hours = now.getHours().toString().padStart(2, '0');
+        let minutes = now.getMinutes().toString().padStart(2, '0');
+        return `${year}-${month}-${day} ${hours}:${minutes}`;
+    };
+
     form.addEventListener("submit", async function (e) {
         e.preventDefault();
         const submitBtn = form.querySelector('button[type="submit"]');
@@ -29,7 +39,8 @@ export function setupRSVP() {
             name: form.name.value?.trim(),
             status: form.status.value?.trim(),
             message: form.message.value?.trim(),
-            date: new Date().toISOString(),
+            // date: new Date().toISOString(),
+            date: getCurrentDateTime(),
             color: "",
         };
 
